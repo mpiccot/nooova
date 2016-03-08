35cb47b7e4dbd70687596372481b5904e02595e9
dan-piccot <piccot@live.unc.edu>
2016-03-07 19:25:44 -0500

    Update Basic Node Program

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
 var ws = require("nodejs-websocket")
- 
+ var connections = [];
 // Scream server example: "hi" -> "HI!!!" 
 var server = ws.createServer(function (conn) {
 	console.log("New connection")
 	conn.on("text", function (str) {
 		console.log("Received "+str)
-		conn.sendText(str.toUpperCase()+"!!!")
+		for (var i = 0; i < connections.length; i++){
+			connections[i].sendText(str.toUpperCase()+"!!!")
+		
+		}
 	})
 	conn.on("close", function (code, reason) {
 		console.log("Connection closed")
 	})
+	connections.push(conn);
 }).listen(8001)
