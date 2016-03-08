6f22237ee512dcb7d035d1baaa82c6cd37922725
dan-piccot <piccot@live.unc.edu>
2016-03-07 20:11:46 -0500

    Update Server Files

--- test.js
+++ test.js
@@ -1,17 +1,39 @@
 var ws = require("nodejs-websocket")
  var connections = [];
+ var currentID = 0;
+
+ 
+ function removeConnection(id){
+	for (var i = 0; i < connections.length; i++){
+		if (connections[i].id == id)
+			connections.splice(i,i);		
+	} 
+ }
+ 
+ function sendMessage(message){
+	for (var i = 0; i < connections.length; i++){
+		connections[i].sendText(message)		
+	} 
+ }
 // Scream server example: "hi" -> "HI!!!" 
 var server = ws.createServer(function (conn) {
 	console.log("New connection")
 	conn.on("text", function (str) {
 		console.log("Received "+str)
-		for (var i = 0; i < connections.length; i++){
-			connections[i].sendText(str.toUpperCase()+"!!!")
-		
-		}
+		var json = {id : conn.id, event: 'Message Recieved', value: str}
+		sendMessage(JSON.stringify(json));
 	})
 	conn.on("close", function (code, reason) {
-		console.log("Connection closed")
+		removeConnection(conn.id);		
 	})
+	conn.on('uncaughtException', function (code, reason) {
+		removeConnection(conn.id);		
+	})
+	conn.on('error', function (err) {
+		console.log(err);
+		conn.close();				
+	})
+	conn.id = currentID;
+	currentID++;
 	connections.push(conn);
 }).listen(8001)
